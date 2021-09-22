9b490688b1c1229f03e08e8cf4775e2c8c012036
Anastasiia Kaplenko <akaplenko@exadel.com>
2021-09-22 22:51:45 +0300

    Task 04: Collect tree leaves

---
+++ tasks/task04/task04.py
@@ -0,0 +1,30 @@
+tree = {
+   "node1": {
+       "node11": {
+           "node111": [1, 2, 3],
+           "node112": [4, 5]
+       },
+       "node12": [6],
+       "node13": [13]
+   },
+   "node2": [7, 8, 9]
+}
+
+def collect_leaves(tree):
+  result = []
+  if isinstance(tree, list):
+    for x in tree:
+      result.append(x)
+  if isinstance(tree, dict):
+    for k, v in tree.items():
+      array = (collect_leaves(v))
+      for item in array:
+        result.append(item)
+  return result
+
+result = collect_leaves(tree)
+print(result)  
+test_array = [0, 7, 13]
+
+assert collect_leaves(tree) == [1, 2, 3, 4, 5, 6, 13, 7, 8, 9]
+assert collect_leaves(test_array) == test_array
