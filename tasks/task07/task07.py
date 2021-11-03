dd97a3e28f5c1e9ab7af4185c1213b41af2ede13
Anastasiia Kaplenko <akaplenko@exadel.com>
2021-11-03 21:35:10 +0200

    added parameters for Order class constructor

--- tasks/task07/task07.py
+++ tasks/task07/task07.py
@@ -1,70 +1,72 @@
 import uuid
 from datetime import date
 
+
 class Good():
-  def __init__(self, name: str, price: float):
-      self.name = name
-      self.id = uuid.uuid4()
-      self.price = price
+    def __init__(self, name: str, price: float):
+        self.name = name
+        self.id = uuid.uuid4()
+        self.price = price
 
 
 class Order():
-    def __init__(self):
-        self.order_id = uuid.uuid4()
-        self.order_date = date.today()
-        self.client_id = uuid.uuid4()
-        self.Goods = []
-        self.price = 0
-    
+    def __init__(self, order_id: uuid.UUID, order_date, client_id: uuid.UUID, goods: list, price=0):
+        self.order_id = order_id
+        self.order_date = order_date
+        self.client_id = client_id
+        self.Goods = goods
+        self.price = price
+
     @property
     def price(self):
-      return sum(g.price for g in self.goods)
+        return sum(g.price for g in self.goods)
 
     @price.setter
-    def price(self, value): 
-      self._price = value
-    
-    def AddGood(self,good : Good):
-      if not type(good) is Good:
-        return
-      self.Goods.append(good)
+    def price(self, value):
+        self._price = value
+
+    def AddGood(self, good: Good):
+        if not type(good) is Good:
+            return
+        self.Goods.append(good)
+
 
 class OrderRepository():
     def __init__(self):
-      self.items = []
-    
-    def add(self, order: Order):
-      self.items.append(order)
-    
-    def get(self, order_id: uuid.UUID):
-      for order in self.items:
-        if order.order_id == order_id:
-          return order
-        return None
-    
-    def delete(self, order_id: uuid.UUID):
-      order = self.get(order_id)
-      self.items.remove(order)
+        self.items = []
 
-    def list(self, n_latest : int = None):
-      if n_latest == None or n_latest >= len(self.items):
-        return self.items
-      return self.items[:n_latest]
-        
+    def add(self, order: Order):
+        self.items.append(order)
+
+    def get(self, order_id: uuid.UUID):
+        for order in self.items:
+            if order.order_id == order_id:
+                return order
+            return None
+
+    def delete(self, order_id: uuid.UUID):
+        order = self.get(order_id)
+        self.items.remove(order)
+
+    def list(self, n_latest: int = None):
+        if n_latest == None or n_latest >= len(self.items):
+            return self.items
+        return self.items[:n_latest]
+
 
 apple = Good("Apple", 10.9)
 cheese = Good("Cheese", 113)
 water = Good("Water", 15)
 
-order1 = Order()
+order1 = Order(uuid.uuid4(), date.today(), uuid.uuid4(), [])
 order1.AddGood(apple)
 order1.AddGood(cheese)
 
-order2 = Order()
+order2 = Order(uuid.uuid4(), date.today(), uuid.uuid4(), [])
 order2.AddGood(apple)
 order2.AddGood(apple)
 
-order3 = Order()
+order3 = Order(uuid.uuid4(), date.today(), uuid.uuid4(), [])
 order3.AddGood(apple)
 order3.AddGood(cheese)
 order3.AddGood(water)
